d73bc26bd71fdfb8cd7c97ed6687c7220ea1ddd3
ThibautRoperch <thibaut.roperch@free.fr>
2016-10-17 11:41:57 +0200

    this corrigés dans props.java

--- src/Propositions.java
+++ src/Propositions.java
@@ -2,25 +2,25 @@ import java.util.HashMap;
 
 public class Propositions {
 
-    protected HashMap _content;
+    protected HashMap content;
 
 	// constructeur
 
     public Propositions() {
-        _content = new HashMap;
+        this->content = new HashMap;
     }
 
 	// autres
 
     public void set(Proposition p) {
-        _content.put(p.get_type(), p.get_value());
+        this->content.put(p.get_type(), p.get_value());
     }
 
     public vois set(String type, String value) {
-        _content.put(type, value);
+        this->content.put(type, value);
     }
 
     public boolean contains(Proposition p) {
-        return _content.containsKey(p.get_type()) && _content.contains(p.get_value());
+        return this->content.containsKey(p.get_type()) && this->content.contains(p.get_value());
     }
 }
